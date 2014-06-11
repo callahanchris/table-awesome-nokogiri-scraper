d36b10825cf1f1363b21b7d194a18f30dacce46d
Chris Callahan <callahan.c@gmail.com>
2014-06-11 18:22:32 -0400

    finish populating ALL_OF_OUR_DATA array

--- scraping_test.rb
+++ scraping_test.rb
@@ -1,50 +1,40 @@
 require 'open-uri'
 require 'nokogiri'
 require 'pry'
+require 'ap'
 
 student_index = Nokogiri::HTML(open('http://ruby005.students.flatironschool.com/'))
-
-
 
 ALL_OF_OUR_DATA = student_index.search("h3 a").collect do |student|
   {student.text =>
    {:profile_page => "http://ruby005.students.flatironschool.com/#{student["href"]}"}} 
 end
 
-p ALL_OF_OUR_DATA
+student_index.search("h3 a").each_with_index do |student, i|
+  profile_page = Nokogiri::HTML(open(ALL_OF_OUR_DATA[i][student.text][:profile_page]))
+  puts "I'm scraping #{student.text}'s profile page!"
+  favorite_quote = profile_page.search("div.textwidget h3").text
+  links_array = profile_page.search("div.social-icons a")
 
-# binding.pry
+  social_links = links_array.collect do |link|
+    link["href"]
+  end
 
-# ALL_OF_OUR_DATA = [
-#                     {
-#                       "Michael" => {
-#                         :profile_page => link
-#                       }
-#                     }, 
-#                   ]
+  twitter = social_links[0]
+  linkedin = social_links[1]
+  github = social_links[2]
 
-  # ALL_OF_OUR_DATA = [
-  #   "Student Name" => {
-    ## h3 > a & name
-    ## <a href="students/MatthiasLoker.html">Matthias Loker</a>
+  if student.text == 'Rebecca Greenblatt'
+    blog = profile_page.search("div.services")[3].search("a")[0].text
+  else
+    blog = profile_page.search("div.services")[4].search("a")[0]['href']
+  end
 
-  #     :profile_page => ruby005.students.flatironschool.com/studentname.html,
-  #     :tagline => "i'm at table awesome", # tagline from profile page
-  #     :links => { # iterate through links that are on profile page
-  #       :github => www.github.com,
-  #       :twitter => twitter.com,
-  #       :blog => www.blog.com,
-  #       :linkedin => www.linkedin.com
-  #     },
-  #     :favorite_cities => ["NYC", "BK"]
-  #   }
-  # ]
-
-
-
-
-
-# When you use Nokogiri to scrape a page it behind the scenes makes an instance of that page and puts it on your system.
-# Because it's an inastance you can run methods on it (check .methods to see what are available.)
-
-# 
+  ALL_OF_OUR_DATA[i][student.text].merge!({
+    :favorite_quote => favorite_quote,
+    :github => github,
+    :twitter => twitter,
+    :blog => blog,
+    :linkedin => linkedin
+  })
+end
